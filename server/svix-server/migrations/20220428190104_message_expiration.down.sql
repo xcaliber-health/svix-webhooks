2e90924c04261123ab02025ae676533ea1a281a8
Thrisha <thrisha.bandi@zemosolabs.com>
2023-01-17 16:44:06 +0530

    Changed the database code

--- server/svix-server/migrations/20220428190104_message_expiration.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE message DROP COLUMN expiration;
-ALTER TABLE message ALTER COLUMN payload SET NOT NULL;
-DROP INDEX message_payload_not_null_pidx;
--- server/svix-server/migrations/20220506181355_remove_updated_at_from_static_entities.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE message DROP COLUMN updated_at;
-ALTER TABLE messageattempt DROP COLUMN updated_at;
